6d8b246310ce9f569b890bad57fc2a7b83d48781
Bill Pugh <pugh@cs.umd.edu>
2019-08-09 15:27:38 -0400

    test central code is working

--- sketches/AchillesCentral/AchillesCentral.ino
+++ sketches/AchillesCentral/AchillesCentral.ino
@@ -1,13 +1,35 @@
+#include <Wire.h>
 
 
-#include <Arduino.h>
 #include "AchillesCentral.h"
+#include "AchillesLog.h"
 
 WedgeData wedges[] = {
-  WedgeData("PhotoSonic", 71, 0),
-  WedgeData("Hexaplexor", 72, 2),
-  WedgeData("HexaplexorHelper", 73, -1),
-  WedgeData("TileMaze", 74, 4)
+  WedgeData("TestWedge", 0x71, 0),
+//  WedgeData("PhotoSonic", 0x71, 0),
+//  WedgeData("Hexaplexor",  0x72, 2),
+//  WedgeData("HexaplexorHelper",  0x73, -1),
+//  WedgeData("TileMaze",  0x74, 4)
 };
 
-const int numWedges = sizeof(wedges)/sizeof(WedgeData);
+const size_t numWedges = sizeof(wedges) / sizeof(WedgeData);
+unsigned long lastActivity;
+int totalPoints;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial ) delay(1);
+  logf("Achilles Central, compiled %s, %s\n",
+       F(__DATE__), F(__TIME__));
+
+  initializeCentral();
+
+}
+
+void loop() {
+  
+  lastActivity = millis();
+   scanWedges(ACTIVE);
+   delay(1000);
+
+}
