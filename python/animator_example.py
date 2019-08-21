dfc0545f654de294cc0abca1278f1382a0017b70
dougtinva <dougtinva@gmail.com>
2019-08-20 22:58:13 -0400

    simplified (and working) example

--- python/animator_example.py
+++ python/animator_example.py
@@ -6,29 +6,31 @@ import time
 
 anim = animator.Animator()
 
-leds = neopixel.NeoPixel(board.D29, 6, auto_write=False)
+tile = neopixel.NeoPixel(board.D29, 6, auto_write=False)
+cross = [True, False, True, True, True, True]
+colorOff = (0, 0, 0)
+
+def draw_pattern(tile, pattern, color):
+    for index, led in enumerate(pattern):
+        if led:
+            tile[index] = color
+        else:
+            tile[index] = colorOff
+        tile.show()
+        print('DRAW PIXEL {}'.format(index))
+        yield 1  # wait 1 second
 
 def draw_pattern_complete():
     print('DRAW PATTERN COMPLETE!')
-
-def draw_pattern(color):
-    for index, pixel in enumerate(leds):
-        # loop over all 6 pixels and set the color
-        for p in range(6):
-            if index == p:
-                pixel = color
-            else:
-                pixel = (0, 0, 0)
-        leds.show()
-        print('DRAW PIXEL {}'.format(index))
-        yield 1 # wait 1 second
 
 # start running the animation non-blocking
 # first argument is the function that makes the animation
 # second argument is a list of arguments to be passed to the function
 # third arugment is a callback function to be called once the animation completes
-anim.start_animation(draw_pattern, [(255, 0, 0)], draw_pattern_complete)
+
+anim.start_animation(draw_pattern, [tile, cross, (255, 0, 0)], draw_pattern_complete)
 
 while True:
     anim.update()
+
     # you can do other things here, like i2c comms!
